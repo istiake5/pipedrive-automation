d20ac6b5c4c0f5b25871cb8b37512b2a4173056a
Sultanul-Arfin <fahimsultan4@gmail.com>
2023-09-19 01:27:52 +0600

    update: 2 endpoints for get all the leads & all the persons

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LeadController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('get-leads', [LeadController::class, 'get_leads'])
+    ->name('leads.get-leads');
+Route::get('get-persons', [LeadController::class, 'get_persons'])
+    ->name('leads.get-persons');
